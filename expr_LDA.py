3ede6ae336d675b01c97abcf7137a9b31382fe85
AllenWLynch <allen.lynch1@outlook.com>
2021-03-22 10:20:02 -0400

    finished scVLXCM

--- expr_LDA.py
+++ expr_LDA.py
@@ -24,7 +24,7 @@ class Decoder(nn.Module):
     # Base class for the decoder net, used in the model
     def __init__(self, num_genes, num_topics, dropout):
         super().__init__()
-        self.beta = nn.Linear(num_topics, num_genes)
+        self.beta = nn.Linear(num_topics, num_genes, bias = False)
         self.bn = nn.BatchNorm1d(num_genes)
         self.drop = nn.Dropout(dropout)
 
@@ -56,14 +56,16 @@ class Encoder(nn.Module):
         return theta_loc, theta_scale
 
 
-class ProdLDA(PyroModule):
+class scVLXCM(nn.Module):
 
-    local_vars = ['theta','expr_rate']
+    local_vars = ['theta']
     global_vars = ['dispersion','dropout']
 
     var_names = local_vars + global_vars
+    variational_vars = var_names + ['beta', 'bias','gamma','bn_mean','bn_var']
 
-    def __init__(self, num_genes,num_topics = 15, initial_counts = 50, dropout = 0.2, hidden = 128):
+    def __init__(self, num_genes,num_topics = 15, initial_counts = 50, 
+        dropout = 0.2, hidden = 128, use_cuda = True):
         super().__init__()
 
         assert(isinstance(initial_counts, int))
@@ -77,26 +79,28 @@ class ProdLDA(PyroModule):
         self.decoder = Decoder(self.num_genes, num_topics, dropout)
         self.encoder = Encoder(self.num_genes, num_topics, hidden, dropout)
 
-    def write_trace(self, filename):
-        logging.info('Writing trace ...')
-        with open(filename, 'wb') as f:
-            pickle.dump(self.trace, f)
+        self.use_cuda = torch.cuda.is_available() and use_cuda
+        logging.info('Using CUDA: {}'.format(str(self.use_cuda)))
+        self.device = torch.device('cuda:0' if self.use_cuda else 'cpu')
 
-    def load_trace(self, filename):
+        self.to(self.device)
+        self.max_prob = torch.tensor([0.99999], requires_grad = False).to(self.device)
 
-        with open(filename, 'rb') as f:
-            self.trace = pickle.load(f)
 
-        self.summarize_params()
+    def write_trace(self, prefix):
+        logging.info('Writing results ...')
+        for varname in self.variational_vars:
+            np.save(prefix + '_{}.npy'.format(varname), getattr(self, varname))        
 
-    def summarize_params(self, metric = np.mean):
-        logging.info('Summarizing params ...')
+    def load_trace(self, prefix):
 
-        for param in self.var_names:
-            self.__setattr__(param, metric(self.trace[param], axis = 0))
-
-    def get_log_expr_rate(self, *args, **kwargs):
-        raise NotImplementedError()
+        for suffix in self.variational_vars:
+            try:
+                filename = prefix + '_{}.npy'.format(suffix)
+                m = np.load(filename)
+                self.__setattr__(suffix, m)
+            except FileNotFoundError:
+                raise Exception('Cannot make model estimator, missing file: {}'.format(filename))
 
     def model(self, raw_expr, encoded_expr, read_depth):
 
@@ -104,8 +108,8 @@ class ProdLDA(PyroModule):
 
         with pyro.plate("genes", self.num_genes):
 
-            dispersion = pyro.sample("dispersion", dist.Gamma(2., 0.5))
-            psi = pyro.sample("dropout", dist.Beta(1., 10.))
+            dispersion = pyro.sample("dispersion", dist.Gamma(torch.tensor(2.).to(self.device), torch.tensor(0.5).to(self.device)) )
+            psi = pyro.sample("dropout", dist.Beta(torch.tensor(1.).to(self.device), torch.tensor(10.).to(self.device)) )
         
         #pyro.module("decoder", self.decoder)
         with pyro.plate("cells", encoded_expr.shape[0]):
@@ -121,7 +125,7 @@ class ProdLDA(PyroModule):
             expr_rate = pyro.deterministic("expr_rate", self.decoder(theta))
 
             mu = torch.multiply(read_depth, expr_rate)
-            p = torch.minimum(mu / (mu + dispersion), torch.tensor([0.99999]))
+            p = torch.minimum(mu / (mu + dispersion), self.max_prob)
 
             pyro.sample('obs', 
                         dist.ZeroInflatedNegativeBinomial(total_count=dispersion, probs=p, gate = psi).to_event(1),
@@ -134,13 +138,13 @@ class ProdLDA(PyroModule):
 
         with pyro.plate("genes", self.num_genes):
             
-            gamma_a = pyro.param("gamma_a", torch.tensor(2.), constraint = constraints.positive)
-            gamma_b = pyro.param("gamma_b", torch.tensor(0.5), constraint = constraints.positive)
+            gamma_a = pyro.param("gamma_a", torch.tensor(2., device = self.device), constraint = constraints.positive)
+            gamma_b = pyro.param("gamma_b", torch.tensor(0.5, device = self.device), constraint = constraints.positive)
             dispersion = pyro.sample("dispersion", dist.Gamma(gamma_a, gamma_b))
 
-            dropout_a = pyro.param("dropout_a", torch.tensor(1.), constraint = constraints.positive)
-            dropout_b = pyro.param("dropout_b", torch.tensor(10.), constraint = constraints.positive)
-            dropout = pyro.sample("dropout", dist.Beta(dropout_a, dropout_b))           
+            dropout_a = pyro.param("dropout_a", torch.tensor(1., device = self.device), constraint = constraints.positive)
+            dropout_b = pyro.param("dropout_b", torch.tensor(10., device = self.device), constraint = constraints.positive)
+            dropout = pyro.sample("dropout", dist.Beta(dropout_a, dropout_b))          
 
 
         with pyro.plate("cells", encoded_expr.shape[0]):
@@ -151,7 +155,8 @@ class ProdLDA(PyroModule):
             theta = pyro.sample(
                 "theta", dist.LogNormal(theta_loc, theta_scale).to_event(1))
 
-    def sample_posterior(self, raw_expr, encoded_expr, read_depth, 
+
+    def summarize_posterior(self, raw_expr, encoded_expr, read_depth, 
         num_samples = 200, attempts = 5):
 
         logging.info('Sampling posterior ...')
@@ -166,11 +171,11 @@ class ProdLDA(PyroModule):
             
             if i == 0:
                 for varname in self.global_vars:
-                    new_samples = samples[varname].detach().numpy()
+                    new_samples = samples[varname].cpu().detach().numpy()
                     trace[varname] = new_samples
             
             for varname in self.local_vars:
-                new_samples = samples[varname].detach().numpy()
+                new_samples = samples[varname].cpu().detach().numpy()
                 
                 if not varname in trace:
                     trace[varname] = []
@@ -181,17 +186,28 @@ class ProdLDA(PyroModule):
         for varname in self.local_vars:
             trace[varname] = np.concatenate(trace[varname], axis = 1)
 
-        trace['beta'] = self.get_beta()
-        trace['bias'] = self.get_bias()
+        for varname in self.var_names:
+            self.__setattr__(varname, np.mean(trace[varname], axis = 0))
 
-        return trace
+        self.beta = self.get_beta()
+        self.gamma = self.get_gamma()
+        self.bias = self.get_bias()
+        self.bn_mean = self.get_bn_mean()
+        self.bn_var = self.get_bn_var()
+
+        return self
 
     @staticmethod
     def softmax(x):
         return np.exp(x)/np.exp(x).sum(-1, keepdims = True)
 
     def predict(self, theta):
-        return self.softmax(np.dot(self.theta, self.trace['beta']) + self.trace['bias'][np.newaxis, :])
+        assert(np.isclose(theta.sum(-1), 1).all()), 'Theta latent var must be composition'
+
+        activation = (np.dot(theta, self.beta) - self.bn_mean)/np.sqrt(self.bn_var)
+
+        return self.softmax(self.gamma * activation + self.bias)
+
 
     def epoch_batch(self, *data, batch_size = 32):
 
@@ -206,6 +222,16 @@ class ProdLDA(PyroModule):
     def train(self, *, raw_expr, encoded_expr, read_depth, num_epochs = 100, 
             batch_size = 32, learning_rate = 1e-3, posterior_samples = 20):
 
+        logging.info('Validating data ...')
+
+        assert(raw_expr.shape == encoded_expr.shape)
+        assert(raw_expr.shape[0] == read_depth.shape[0])
+        assert(len(read_depth.shape) == 1)
+
+        raw_expr = torch.tensor(raw_expr).to(self.device)
+        encoded_expr = torch.tensor(encoded_expr).to(self.device)
+        read_depth = torch.tensor(read_depth[:, np.newaxis]).to(self.device)
+
         logging.info('Initializing model ...')
 
         pyro.clear_param_store()
@@ -213,25 +239,23 @@ class ProdLDA(PyroModule):
         optimizer = Adam(adam_params)
         svi = SVI(self.model, self.guide, optimizer, loss=TraceMeanField_ELBO())
 
-        raw_expr = torch.tensor(raw_expr)
-        encoded_expr = torch.tensor(encoded_expr)
-        read_depth = torch.tensor(read_depth[:, np.newaxis])
-
+        logging.info('Training ...')
         num_batches = read_depth.shape[0]//batch_size
         bar = trange(num_epochs)
 
-        logging.info('Training ...')
-        for epoch in bar:
-            running_loss = 0.0
-            for batch in self.epoch_batch(raw_expr, encoded_expr, read_depth, batch_size = batch_size):
-                loss = svi.step(*batch)
-                running_loss += loss / batch_size
+        try:
+            for epoch in bar:
+                running_loss = 0.0
+                for batch in self.epoch_batch(raw_expr, encoded_expr, read_depth, batch_size = batch_size):
+                    loss = svi.step(*batch)
+                    running_loss += loss / batch_size
 
-            bar.set_postfix(epoch_loss='{:.2e}'.format(running_loss))
+                bar.set_postfix(epoch_loss='{:.2e}'.format(running_loss))
+        except KeyboardInterrupt:
+            logging.error('Interrupted training.')
 
-        self.trace = self.sample_posterior(raw_expr, encoded_expr, read_depth, 
+        self.summarize_posterior(raw_expr, encoded_expr, read_depth, 
                 num_samples = posterior_samples)
-        self.summarize_params()
 
         return self
 
@@ -239,22 +263,33 @@ class ProdLDA(PyroModule):
         # beta matrix elements are the weights of the FC layer on the decoder
         return self.decoder.beta.weight.cpu().detach().T.numpy()
 
+    def get_gamma(self):
+        return self.decoder.bn.weight.cpu().detach().numpy()
+    
     def get_bias(self):
-        return self.decoder.beta.bias.cpu().detach().T.numpy()
+        return self.decoder.bn.bias.cpu().detach().numpy()
+
+    def get_bn_mean(self):
+        return self.decoder.bn.running_mean.cpu().detach().numpy()
+
+    def get_bn_var(self):
+        return self.decoder.bn.running_var.cpu().detach().numpy()
 
 def main(*,raw_expr, encoded_expr, read_depth, save_file, topics = 32, hidden_layers=128,
-    learning_rate = 1e-3, epochs = 100, batch_size = 32, posterior_samples = 20):
+    learning_rate = 1e-3, epochs = 100, batch_size = 32, posterior_samples = 20, initial_counts = 50):
 
     raw_expr = np.load(raw_expr)
     encoded_expr = np.load(encoded_expr)
     read_depth = np.load(read_depth)
 
-    assert(raw_expr.shape == encoded_expr.shape)
-    assert(raw_expr.shape[0] == read_depth.shape[0])
-    assert(len(read_depth.shape) == 1)
-
-    rna_topic = ProdLDA(raw_expr.shape[-1], num_topics = topics, dropout = 0.2, hidden = hidden_layers)
+    seed = 2556
+    torch.manual_seed(seed)
+    pyro.set_rng_seed(seed)
     
+
+    rna_topic = scVLXCM(raw_expr.shape[-1], num_topics = topics, dropout = 0.2, 
+        hidden = hidden_layers, initial_counts = initial_counts)
+
     rna_topic.train(
         raw_expr = raw_expr,
         encoded_expr = encoded_expr,
@@ -262,12 +297,10 @@ def main(*,raw_expr, encoded_expr, read_depth, save_file, topics = 32, hidden_la
         num_epochs = epochs,
         batch_size = batch_size,
         learning_rate = learning_rate,
-        posterior_samples = posterior_samples,
+        posterior_samples = posterior_samples
     )
 
     rna_topic.write_trace(save_file)
 
 if __name__ == "__main__":
     fire.Fire(main)
-
-
